ce6d7c27b90fd2ac904899379fa32b8102749bc2
Tarek Ziade <tarek@ziade.org>
2017-01-10 11:27:12 +0100

    linked CONTRIBUTORS in the long description

--- CONTRIBUTORS.rst
+++ CONTRIBUTORS.rst
@@ -1,5 +1,9 @@
 Contributors
 ============
 
-* Tarek Ziade <tarek_AT_ziade.org>
+Project created by Tarek Ziade <tarek_AT_ziade.org>
+
+Contributors in order of appearance:
+
 * Lucas Cimon <lucas.cimon_AT_gmail.com>
+
--- setup.py
+++ setup.py
@@ -6,11 +6,19 @@ with open('requirements.txt') as f:
     deps = [dep for dep in f.read().split('\n') if dep.strip() != '']
     install_requires = deps
 
+with open('README.rst') as f:
+    _DESC = f.read() + '\n\n'
+
+
+with open('CONTRIBUTORS.rst') as f:
+    _DESC += f.read()
+
 
 setup(name='smwogger',
       version="0.1",
       packages=find_packages(),
       description="Smoke Test tool",
+      long_description=_DESC,
       include_package_data=True,
       zip_safe=False,
       install_requires=install_requires,
