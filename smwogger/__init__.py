f9be418e8d20e8a1d5699fbe353fcbea9e5778a2
Tarek Ziade <tarek@ziade.org>
2017-03-28 09:16:03 +0200

    expose API in top module

--- smwogger/__init__.py
+++ smwogger/__init__.py
@@ -3,3 +3,9 @@ import logging
 
 logger = logging.getLogger('smwogger')    # NOQA
 logging.basicConfig(level=logging.WARNING)
+
+try:
+    from smwogger.api import API          # NOQA
+except ImportError:
+    # first import (setuptools)
+    pass                                  # NOQA
